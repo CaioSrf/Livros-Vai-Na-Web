f234adf708e4ad99c860afda2187e9ae9ba89e9b
CaioSrf <caiosrfreitas@gmail.com>
2024-12-04 01:22:30 -0300

    erro Pages

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Inicio from '../../pages/inicio/Inicio';
+import Inicio from '../../Pages/inicio/Inicio';
 import LivrosDoados from '../../Pages/livrosDoados/LivrosDoados';
 import QueroDoar from '../../Pages/queroDoar/QueroDoar';
 import logoLivro from '../../assets/logoLivro.png';
